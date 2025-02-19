af18b55479dc675894892669555b1e66c4e3c51f
shereenmagdy21 <shereen.magdy@roqay.com>
2025-02-19 10:52:28 +0200

    Initial commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,53 @@
+<script setup>
+import { ref } from 'vue'
+
+const selectedLanguage = ref('en') // Default language
+
+const languages = [
+  { code: 'en', name: 'English' },
+  { code: 'fr', name: 'French' },
+  { code: 'es', name: 'Spanish' }
+]
+</script>
+
+<template>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+          <router-link to="/" class="navbar-brand">Food Menu</router-link>
+          <div>
+          <select id="lang" v-model="selectedLanguage">
+            <option v-for="lang in languages" :key="lang.code" :value="lang.code">
+              {{ lang.name }}
+            </option>
+          </select>
+          </div>
+          <div>
+          <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
+            <i class="fa-solid fa-bag-shopping text-sm"></i>
+          </button>
+
+          <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+            <div class="offcanvas-header">
+              <h5 class="offcanvas-title" id="offcanvasExampleLabel">Offcanvas</h5>
+              <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body">
+              <div>
+                Some text as placeholder. In real life you can have the elements you have chosen. Like, text, images, lists, etc.
+              </div>
+              <div class="dropdown mt-3">
+                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown">
+                  Dropdown button
+                </button>
+                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                  <li><a class="dropdown-item" href="#">Action</a></li>
+                  <li><a class="dropdown-item" href="#">Another action</a></li>
+                  <li><a class="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          </div>
+      </div>
+  </nav>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <navbar />
+</template>
+
+<script>
+import { ref } from 'vue'
+import { RouterLink, RouterView } from 'vue-router'
+import Navbar from '../components/Navbar.vue'
+
+export default {
+    name: 'home',
+  components: {
+    Navbar,
+    RouterLink,
+    RouterView
+  }
+}
+</script>
